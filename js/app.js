5d65c95b2c4c1919e7568756adbea051d585f400
Ian <iangaroian@gmail.com>
2015-07-10 11:27:27 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+
+var myApp = angular.module('myApp',[]);
+	myApp.controller('AccountController', ['$scope', function($scope){
+
+		
+		$scope.balance = 100;
+		$scope.add = add;
+		$scope.subtract = subtract;
+		
+		// credits account balance when a user inputs a Deposit amount.
+		function add(num) {
+			return $scope.balance += parseInt(num);
+		}
+		// Debits account balance when a user inputs a Debit amount.
+		function subtract(num) {
+			console.log(num);
+			return $scope.balance -= parseInt(num);
+		}
+	
+	}]);
+
+
+
+
+
+
+
+
